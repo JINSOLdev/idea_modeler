cd2ed3801317b5b5092616a111cd2e03286bf754
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-05-16 13:10:13 +0900

    feat: FileSystem(1)

---
+++ idea-rpa-modeler/frontend/src/components/designer/modeling/elements/keyword/FileSystem/CopyDirectoryPanel.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-card flat>
+            <v-card-title class="subtitle-1">
+                Input Arguments
+            </v-card-title>
+
+            <v-card-text>
+                <path-field
+                        :label="'Source'"
+                        :hint="'Path to source directory'"
+                        :value.sync="value.property.source"
+                ></path-field>
+                <path-field
+                        :label="'Destination'"
+                        :hint="'Destination path'"
+                        :value.sync="value.property.destination"
+                ></path-field>
+            </v-card-text>
+        </v-card>
+
+    </div>    
+</template>
+
+<script lang="ts">
+    import { Component, Mixins } from "vue-property-decorator"
+    import KeywordPanel from '../../panels/KeywordPanel.vue'
+
+    @Component({
+        components: {
+            KeywordPanel,
+        }
+    })
+    export default class CopyDirectory extends Mixins(KeywordPanel) {
+    }
+</script>
