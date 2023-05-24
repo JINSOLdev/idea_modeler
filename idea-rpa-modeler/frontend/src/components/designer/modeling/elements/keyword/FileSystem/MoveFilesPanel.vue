9a2cb9cc1e22cbf8e2cec6bf0e91dd29957fffdf
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-05-24 13:05:29 +0900

    feat : FileSystems

---
+++ idea-rpa-modeler/frontend/src/components/designer/modeling/elements/keyword/FileSystem/MoveFilesPanel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-card flat>
+            <v-card-title class="subtitle-1">
+                Input Arguments
+            </v-card-title>
+
+            <v-card-text>
+                <path-field
+                        :label="'Source'"
+                        :hint="'File(s) to move'"
+                        :value.sync="value.property.source"
+                ></path-field>
+
+                <path-field
+                        :label="'Destination'"
+                        :hint="'Destination path'"
+                        :value.sync="value.property.destination"
+                ></path-field>
+               
+            </v-card-text>
+        </v-card>
+
+      
+
+    </div>    
+</template>
+
+<script lang="ts">
+    import { Component, Mixins } from "vue-property-decorator"
+    import KeywordPanel from '../../panels/KeywordPanel.vue'
+
+    @Component({
+        components: {
+            KeywordPanel,
+        }
+    })
+    export default class MoveFilesPanel extends Mixins(KeywordPanel) {
+    }
+</script>
---
+++ idea-rpa-modeler/frontend/src/components/designer/modeling/elements/keyword/FileSystem/RemoveFilePanel.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <v-card flat>
+            <v-card-title class="subtitle-1">
+                Input Arguments
+            </v-card-title>
+
+            <v-card-text>
+                <path-field
+                        :label="'Path'"
+                        :hint="'Path to file'"
+                        :value.sync="value.property.path"
+                ></path-field>
+
+
+            </v-card-text>
+        </v-card>
+
+  
+
+    </div>    
+</template>
+
+<script lang="ts">
+    import { Component, Mixins } from "vue-property-decorator"
+    import KeywordPanel from '../../panels/KeywordPanel.vue'
+
+    @Component({
+        components: {
+            KeywordPanel,
+        }
+    })
+    export default class RemoveFilePanel extends Mixins(KeywordPanel) {
+    }
+</script>
