34fe224a1eeaf43c5ab88bc406e6e3a39df53617
Jinsol Kim <jinsol0323@naver.com>
2023-05-30 13:19:42 +0900

    Add : FileSystem 추가 등록

---
+++ idea-rpa/frontend/src/components/designer/modeling/elements/keyword/FileSystem/WaitUntilRemovedPanel.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-card flat>
+            <v-card-title class="subtitle-1">
+                Input Arguments
+            </v-card-title>
+
+            <v-card-text>
+                <path-field
+                        :label="'Path'"
+                        :hint="'Path to file'"
+                        :value.sync="value.property.path"
+                ></path-field>
+
+                <variables-field
+                        :label="'Timeout'"
+                        :hint="'Maximum wait time (in seconds)'"
+                        :value.sync="value.property.timeout"
+                ></variables-field>
+
+
+            </v-card-text>
+        </v-card>
+
+  
+
+    </div>    
+</template>
+
+<script lang="ts">
+    import { Component, Mixins } from "vue-property-decorator"
+    import KeywordPanel from '../../panels/KeywordPanel.vue'
+
+    @Component({
+        components: {
+            KeywordPanel,
+        }
+    })
+    export default class WaitUntilRemovedPanel extends Mixins(KeywordPanel) {
+    }
+</script>
