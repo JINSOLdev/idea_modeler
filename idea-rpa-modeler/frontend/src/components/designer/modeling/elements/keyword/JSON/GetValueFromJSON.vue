2732db45f58697365c5d5c2a46b3ea4684dfbb7b
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-06-05 15:29:06 +0900

    feat : JSON

--- idea-rpa-modeler/frontend/src/components/designer/modeling/elements/keyword/JSON/GetValueFromJSON.vue
+++ idea-rpa-modeler/frontend/src/components/designer/modeling/elements/keyword/JSON/GetValueFromJSON.vue
@@ -7,9 +7,11 @@
             KeywordElement,
         }
     })
-    export default class AddToJSON extends Mixins(KeywordElement) {
+    export default class DeleteFromJSON extends Mixins(KeywordElement) {
         mounted() {
-            
+            this.value.property.returnVal = [{
+                value: 'value'
+            }]
         }
     }
 </script>
