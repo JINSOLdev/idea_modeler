12173cf1bc8cf9fded97d6549baf338d2dd49226
Jinsol Kim <jinsol032360@gmail.com>
2023-06-15 15:04:15 +0900

    fix. String

--- idea-rpa/frontend/src/components/designer/modeling/elements/keyword/String/ReplaceString.vue
+++ idea-rpa/frontend/src/components/designer/modeling/elements/keyword/String/ReplaceString.vue
@@ -13,6 +13,21 @@
         }
 
         init() {
+            var keys = Object.keys(this.value.property)
+            if (!keys || keys.length < 1) {
+                this.value.property = new Map()
+                this.$set(this.value.property, "string", null)
+                this.$set(this.value.property, "search_for", null)
+                this.$set(this.value.property, "replace_with", null)
+                this.$set(this.value.property, "count", -1)
+                this.$set(this.value.property, "returnVal",  [{
+                    defaultValue: "result"
+                }])
+            }
+
+            if (!this.value.property.hasOwnProperty("count")) {
+                this.$set(this.value.property, "count", -1)
+            }
             if (!this.value.property.hasOwnProperty("returnVal")) {
                 this.$set(this.value.property, "returnVal", [{
                     defaultValue: "result"
--- idea-rpa/frontend/src/components/designer/modeling/elements/keyword/String/SplitString.vue
+++ idea-rpa/frontend/src/components/designer/modeling/elements/keyword/String/SplitString.vue
@@ -13,6 +13,23 @@
         }
 
         init() {
+            var keys = Object.keys(this.value.property)
+            if (!keys || keys.length < 1) {
+                this.value.property = new Map()
+                this.$set(this.value.property, "string", null)
+                this.$set(this.value.property, "separator", "None")
+                this.$set(this.value.property, "max_split", -1)
+                this.$set(this.value.property, "returnVal",  [{
+                    defaultValue: "result"
+                }])
+            }
+
+            if (!this.value.property.hasOwnProperty("separator")) {
+                this.$set(this.value.property, "separator", "None")
+            }
+            if (!this.value.property.hasOwnProperty("max_split")) {
+                this.$set(this.value.property, "max_split", -1)
+            }
             if (!this.value.property.hasOwnProperty("returnVal")) {
                 this.$set(this.value.property, "returnVal", [{
                     defaultValue: "result"
